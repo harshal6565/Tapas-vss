cd38e72d2ae6f9cf6bb2764c695394e0b7c9347d
Harshal Korade <harshalkorade6565@gmail.com>
2023-02-20 17:29:17 +0530

    first commit

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"Enter first number :"<<endl;
+    cin>>a;
+    cout<<"Enter second number :"<<endl;
+    cin>>b;
+    cout<<"the sum is : "<<a+b<<endl;
+
+
+    return 0;
+}
